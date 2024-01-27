a7e8e68999ed21ef777bc7a309db9baca017a180
Peter Morrison <petermorrison250@gmail.com>
2024-01-27 12:04:29 +0000

    Update main to call services

---
+++ Src/main.c
@@ -0,0 +1,31 @@
+#include <gb/gb.h>
+#include <string.h>
+#include <stdio.h>
+#include "./Models/GameModel.h"
+#include "./Services/CoreService.h"
+#include "./Services/PerformanceService.h"
+#include "./Services/ExploreService.h"
+
+void init_game(struct GameModel* Game)
+{
+    Game->IsAnimating = FALSE;
+    Game->IsPerformanceMode = FALSE;
+    Game->CurrentScore = 0;
+    Game->X = 0;
+    Game->Y = 0;
+    Game->IsDialogVisible = FALSE;
+    Game->OpenDialogLines = 0;
+}
+
+void main()
+{
+    fade_in();
+    struct GameModel Game;
+    init_game(&Game);
+
+    while (1)
+    {   
+        explore_main(&Game);
+        //performance_main(&Game);
+    }
+}
