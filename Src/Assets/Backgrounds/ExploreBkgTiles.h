55ea34dd98d98f653d15274ec399b75fe9a77012
Peter Morrison <petermorrison250@gmail.com>
2024-01-27 18:38:21 +0000

    Add tiles and map for barracks

--- Src/Assets/Backgrounds/ExploreBkgTiles.h
+++ Src/Assets/Backgrounds/ExploreBkgTiles.h
@@ -10,7 +10,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 54
+  Tiles                : 0 to 62
 
   Palette colors       : None.
   SGB Palette          : None.
